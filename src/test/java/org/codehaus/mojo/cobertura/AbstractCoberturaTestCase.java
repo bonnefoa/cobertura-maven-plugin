1eba9685a5dc9d7b0c9e59cc4941ac6a9204609d
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-04-25 10:53:05 +0200

    Test refactoring : Extract method

--- src/test/java/org/codehaus/mojo/cobertura/AbstractCoberturaTestCase.java
+++ src/test/java/org/codehaus/mojo/cobertura/AbstractCoberturaTestCase.java
@@ -16,122 +16,101 @@ package org.codehaus.mojo.cobertura;
  * limitations under the License.
  */
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.codehaus.mojo.cobertura.stubs.ArtifactStub;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Base TestCase for all Cobertura Tests.
- * 
+ *
  * @author Joakim Erdfelt
  */
 public abstract class AbstractCoberturaTestCase
-    extends AbstractMojoTestCase
+        extends AbstractMojoTestCase
 {
-    protected void assertArtifactExists( Artifact artifact )
+    protected void assertArtifactExists(Artifact artifact)
     {
-        if ( artifact == null )
+        if (artifact == null)
         {
-            fail( "Artifact is null." );
+            fail("Artifact is null.");
         }
 
-        if ( artifact.getFile() == null )
+        if (artifact.getFile() == null)
         {
-            fail( "Artifact.file is not defined for " + artifact );
+            fail("Artifact.file is not defined for " + artifact);
         }
 
-        if ( !artifact.getFile().exists() )
+        if (!artifact.getFile().exists())
         {
-            fail( "Artifact " + artifact.getFile().getAbsolutePath() + " file does not exist." );
+            fail("Artifact " + artifact.getFile().getAbsolutePath() + " file does not exist.");
         }
     }
 
-    protected List getPluginClasspath()
-        throws Exception
+    protected List<Artifact> getPluginClasspath()
+            throws Exception
     {
-        String localRepository = System.getProperty( "localRepository" );
+        String localRepository = System.getProperty("localRepository");
+        if (localRepository == null)
+        {
+            localRepository = System.getenv("MVN_REPO");
+        }
 
-        assertNotNull( "System.property(localRepository) should not be null.", localRepository );
+        assertNotNull("System.property(localRepository) should not be null.", localRepository);
 
-        List pluginClasspath = new ArrayList();
+        List<Artifact> pluginClasspath = new ArrayList<Artifact>();
 
         Artifact artifact;
 
-        // artifact = new ArtifactStub();
-        // artifact.setFile( new File( PlexusTestCase.getBasedir() + "/target/classes" ) );
-        // assertArtifactExists( artifact );
-        // pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("net.sourceforge.cobertura", "cobertura", "1.9.4.1", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "net.sourceforge.cobertura" );
-        artifact.setArtifactId( "cobertura" );
-        artifact.setVersion( "1.9.4.1" );
-        artifact.setFile( new File( localRepository + "/net/sourceforge/cobertura/cobertura/1.9.4.1/cobertura-1.9.4.1.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("log4j", "log4j", "1.2.9", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "log4j" );
-        artifact.setArtifactId( "log4j" );
-        artifact.setVersion( "1.2.9" );
-        artifact.setFile( new File( localRepository + "/log4j/log4j/1.2.9/log4j-1.2.9.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("org.apache.ant", "ant", "1.7.0", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "org.apache.ant" );
-        artifact.setArtifactId( "ant" );
-        artifact.setVersion( "1.7.0" );
-        artifact.setFile( new File( localRepository + "/org/apache/ant/ant/1.7.0/ant-1.7.0.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("org.apache.ant", "ant-launcher", "1.7.0", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "org.apache.ant" );
-        artifact.setArtifactId( "ant-launcher" );
-        artifact.setVersion( "1.7.0" );
-        artifact.setFile( new File( localRepository + "/org/apache/ant/ant-launcher/1.7.0/ant-launcher-1.7.0.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("oro", "oro", "2.0.8", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "oro" );
-        artifact.setArtifactId( "oro" );
-        artifact.setVersion( "2.0.8" );
-        artifact.setFile( new File( localRepository + "/oro/oro/2.0.8/oro-2.0.8.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("asm", "asm", "3.0", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "asm" );
-        artifact.setArtifactId( "asm" );
-        artifact.setVersion( "3.0" );
-        artifact.setFile( new File( localRepository + "/asm/asm/3.0/asm-3.0.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("asm", "asm-tree", "3.0", localRepository, "jar");
+        pluginClasspath.add(artifact);
 
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "asm" );
-        artifact.setArtifactId( "asm-tree" );
-        artifact.setVersion( "3.0" );
-        artifact.setFile( new File( localRepository + "/asm/asm-tree/3.0/asm-tree-3.0.jar" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
-
-        artifact = new ArtifactStub();
-        artifact.setGroupId( "net.sourceforge.cobertura" );
-        artifact.setArtifactId( "cobertura-runtime" );
-        artifact.setVersion( "1.9.4.1" );
-        artifact.setFile( new File( localRepository +
-            "/net/sourceforge/cobertura/cobertura-runtime/1.9.4.1/cobertura-runtime-1.9.4.1.pom" ) );
-        assertArtifactExists( artifact );
-        pluginClasspath.add( artifact );
+        artifact =
+                createArtifact("net.sourceforge.cobertura", "cobertura-runtime", "1.9.4.1", localRepository, "pom");
+        pluginClasspath.add(artifact);
 
         return pluginClasspath;
+    }
+
+    private Artifact createArtifact(String groupId, String artifactId, String version, String localRepository, String packaging)
+    {
+        ArtifactStub artifact = new ArtifactStub();
+        artifact.setGroupId(groupId);
+        artifact.setArtifactId(artifactId);
+        artifact.setVersion(version);
+        String fullPath = localRepository +
+                String.format("/%s/%s/%s/%s-%s.%s", groupId.replaceAll("\\.", "/"), artifactId, version, artifactId, version, packaging);
+        artifact.setFile(new File(fullPath));
+        assertArtifactExists(artifact);
+        return artifact;
     }
 
 }
